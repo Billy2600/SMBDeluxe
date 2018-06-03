04fda5e59fc32eedb32e9c2f02b1e9eaa8c19fbb
Billy <billymcfly@gmail.com>
2018-06-02 22:54:49 -0600

    Add ability to move rectangle

--- ParadigmED/MainWindow.xaml.cs
+++ ParadigmED/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace ParadigmED
     /// </summary>
     public partial class MainWindow : Window
     {
+        Rectangle selectedRect = null;
+        Brush oldFill = null; // Store fill of rectangle, to return it to normal after moving it
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Drag rectangles once they've been selected
+        private void DrawingCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (selectedRect == null)
+                return;
+
+            Canvas.SetTop(selectedRect, e.GetPosition(DrawingCanvas).Y - selectedRect.Height / 2);
+            Canvas.SetLeft(selectedRect, e.GetPosition(DrawingCanvas).X - selectedRect.Width / 2);
+        }
+
+        private void Rectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle rect = sender as Rectangle;
+
+            oldFill = rect.Fill;
+            rect.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            selectedRect = rect;
+        }
+
+        private void Rectangle_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle rect = sender as Rectangle;
+
+            selectedRect = null;
+            rect.Fill = oldFill;
         }
     }
 }
